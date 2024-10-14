f6d4fb4e40352d86cad1666ed1b3b5880124d117
Ccyhyym <1824668442@qq.com>
2024-10-14 17:09:11 +0800

    updateall

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum CadrState
+{
+    Cooling,
+    WaitingSun,
+    Ready
+}
+
+public class Card : MonoBehaviour
+{
+    // 冷却 可以被点击 不可用
+
+}
