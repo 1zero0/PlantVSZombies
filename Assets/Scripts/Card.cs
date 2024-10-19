3791b7a31fa63d27ff3752c61ddf83782ee25109
Ccyhyym <1824668442@qq.com>
2024-10-19 21:35:04 +0800

    UpdateALL

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,7 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum CardState
+{
+    Cooling,
+    WaitingSun,
+    Ready
+}
+
 public class Card : MonoBehaviour
 {
-    // 
+    // 冷却 可以被点击 不可用
+    private CardState cardState = CardState.Cooling;
+
+    private void Update()
+    {
+        switch (cardState)
+        {
+            case CardState.Cooling:
+                CoolingUpdate();
+                break;
+            case CardState.WaitingSun:
+                WaitingSunUpdate();
+                break;
+            case CardState.Ready:
+                ReadyUpdate();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void CoolingUpdate()
+    {
+
+    }
+    void WaitingSunUpdate()
+    {
+
+    }
+    void ReadyUpdate()
+    {
+
+    }
 }
