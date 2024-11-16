1f90036a8115980e59f69d8219acd39d0bd3a54c
Ccyhyym <1824668442@qq.com>
2024-11-16 20:28:01 +0800

    UpdateALL

--- Assets/Scripts/Sun.cs
+++ Assets/Scripts/Sun.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
+
 public class Sun : MonoBehaviour
 {
-    public void JumpTo(Vector3 positon)
+    public float moveDuration = 1;
+    public void JumpTo(Vector3 targetPos)
     {
-        transform.DOPath()
+        Vector3 centerPos = (transform.position + targetPos) / 2;
+        float distance = Vector3.Distance(transform.position, targetPos);
+
+        centerPos.y += (distance / 2);
+        // DOPath的参数：路径，时间，曲线类型
+        transform.DOPath(new Vector3[] { transform.position, centerPos, targetPos },
+            moveDuration, PathType.CatmullRom).SetEase(Ease.OutQuad);   // 表示先快后慢
     }
 }
